8b528134b3c6f8b31e62e2c9653e703b7e3335e3
dajomu <mundaymeister@gmail.com>
2015-11-29 21:19:02 +0000

    Added egg icons to animation and changed social media links

--- dev/assets/js/lib/quicksketch.js
+++ dev/assets/js/lib/quicksketch.js
@@ -1,7 +1,7 @@
 ;(function() {
-	
+
 	"use strict";
-		
+
 	// has property and extend jibbed from coffeescript
   var root = window || this;
   var _hasProp = {}.hasOwnProperty;
@@ -15,15 +15,15 @@
 	}
 
 	/* ---------------------------------------
-	
+
 		 Config
-	
+
 	--------------------------------------- */
 
 	var config, eventMap;
-	
+
 	root.QS = {};
-	
+
 	// defaults config for when the QuickSketch instantiates
 	config = {
 		container: document.body,
@@ -33,16 +33,16 @@
 
 	// Interaction events mapping ready
 	eventMap = ["mousedown", "mouseup", "mousemove", "touchstart", "touchend", "touchmove"];
-		
+
 	/* ---------------------------------------
-	
+
 		 Utilities
-	
-	--------------------------------------- */	
+
+	--------------------------------------- */
 
 	// Vector2D (Factory)
 	root.Vector2D = {};Vector2D.create=function(){var e=[];e[0]=0;e[1]=0;return e};Vector2D.clone=function(e){var t=[];t[0]=e[0];t[1]=e[1];return t};Vector2D.fromValues=function(e,t){var n=[];n[0]=e;n[1]=t;return n};Vector2D.copy=function(e,t){e[0]=t[0];e[1]=t[1];return e};Vector2D.set=function(e,t,n){e[0]=t;e[1]=n;return e};Vector2D.add=function(e,t,n){e[0]=t[0]+n[0];e[1]=t[1]+n[1];return e};Vector2D.subtract=function(e,t,n){e[0]=t[0]-n[0];e[1]=t[1]-n[1];return e};Vector2D.sub=Vector2D.subtract;Vector2D.multiply=function(e,t,n){e[0]=t[0]*n[0];e[1]=t[1]*n[1];return e};Vector2D.mult=Vector2D.multiply;Vector2D.divide=function(e,t,n){e[0]=t[0]/n[0];e[1]=t[1]/n[1];return e};Vector2D.div=Vector2D.divide;Vector2D.min=function(e,t,n){e[0]=Math.min(t[0],n[0]);e[1]=Math.min(t[1],n[1]);return e};Vector2D.max=function(e,t,n){e[0]=Math.max(t[0],n[0]);e[1]=Math.max(t[1],n[1]);return e};Vector2D.scale=function(e,t,n){e[0]=t[0]*n;e[1]=t[1]*n;return e};Vector2D.scaleAndAdd=function(e,t,n,r){e[0]=t[0]+n[0]*r;e[1]=t[1]+n[1]*r;return e};Vector2D.distance=function(e,t){var n=t[0]-e[0];var r=t[1]-e[1];return Math.sqrt(n*n+r*r)};Vector2D.dist=Vector2D.distance;Vector2D.squaredDistance=function(e,t){var n=t[0]-e[0];var r=t[1]-e[1];return n*n+r*r};Vector2D.sqrDist=Vector2D.squaredDistance;Vector2D.length=function(e){var t=e[0];var n=e[1];return Math.sqrt(t*t+n*n)};Vector2D.len=Vector2D.length;Vector2D.squaredLength=function(e){var t=e[0];var n=e[1];return t*t+n*n};Vector2D.sqrLen=Vector2D.squaredLength;Vector2D.negate=function(e,t){e[0]=-t[0];e[1]=-t[1];return e};Vector2D.limit=function(e,t,n){var r=t[0],i=t[1];var s=r*r+i*i;if(s>n*n&&s>0){e[0]=r;e[1]=i;Vector2D.normalize(e,e);Vector2D.scale(e,e,n)}return e};Vector2D.normalize=function(e,t){var n=t[0];var r=t[1];var i=n*n+r*r;if(i>0){i=1/Math.sqrt(i);e[0]=t[0]*i;e[1]=t[1]*i}return e};Vector2D.dot=function(e,t){return e[0]*t[0]+e[1]*t[1]};Vector2D.cross=function(e,t,n){var r=t[0]*n[1]-t[1]*n[0];e[0]=e[1]=0;e[2]=r;return e};Vector2D.lerp=function(e,t,n,r){var i=t[0];var s=t[1];e[0]=i+r*(n[0]-i);e[1]=s+r*(n[1]-s);return e};Vector2D.random=function(e,t){t=t||1;var n=Math.random()*2*Math.PI;e[0]=Math.cos(n)*t;e[1]=Math.sin(n)*t;return e};Vector2D.transformMat2=function(e,t,n){var r=t[0];var i=t[1];e[0]=n[0]*r+n[2]*i;e[1]=n[1]*r+n[3]*i;return e};Vector2D.transformMat2d=function(e,t,n){var r=t[0];var i=t[1];e[0]=n[0]*r+n[2]*i+n[4];e[1]=n[1]*r+n[3]*i+n[5];return e};Vector2D.transformMat3=function(e,t,n){var r=t[0];var i=t[1];e[0]=n[0]*r+n[3]*i+n[6];e[1]=n[1]*r+n[4]*i+n[7];return e};Vector2D.transformMat4=function(e,t,n){var r=t[0];var i=t[1];e[0]=n[0]*r+n[4]*i+n[12];e[1]=n[1]*r+n[5]*i+n[13];return e};Vector2D.forEach=function(){var e=Vector2D.create();return function(t,n,r,i,s,o){var u,a;if(!n){n=2}if(!r){r=0}if(i){a=Math.min(i*n+r,t.length)}else{a=t.length}for(u=r;u<a;u+=n){e[0]=t[u];e[1]=t[u+1];s(e,e,o);t[u]=e[0];t[u+1]=e[1]}return t}}();Vector2D.str=function(e){return"Vector2D("+e[0]+", "+e[1]+")"};if(typeof exports!=="undefined"){exports.Vector2D=Vector2D};
-	
+
 	function getEvents(context) {
 		var events = [], e;
 		for (var key in context.__proto__) {
@@ -51,42 +51,42 @@
 		}
 		return events;
 	};
-	
+
 	/* ---------------------------------------
-	
+
 		 Constructor
-	
+
 	--------------------------------------- */
 
-	QS = function(options) {			
+	QS = function(options) {
 
 		var _i, events;
 		var canvas = this.container ? document.getElementById(this.container) : undefined;
 
-		this.attrs = options ? options : {};	
-		if (this.draggable === undefined) this.draggable = false;		
+		this.attrs = options ? options : {};
+		if (this.draggable === undefined) this.draggable = false;
 		this.size = this.size ? this.size : [window.innerWidth,window.innerHeight];
-		
+
 		// return setup
 		if (this.setup) {
 			this.setup.apply(this, arguments);
 		}
-		
+
 		// fire draw to loop
 		if (this.draw) { this.loop(); }
 
-		$(window).resize(function() {				
+		$(window).resize(function() {
 			console.log("resizing");
-			this.resize(); 
+			this.resize();
 		}.bind(this));
 	};
 
 	/* ---------------------------------------
-	
+
 		 Private
-	
+
 	--------------------------------------- */
-	
+
 	QS.prototype.events = [];
 
 	QS.prototype.loop = function() {
@@ -97,11 +97,11 @@
 	};
 
 	/* ---------------------------------------
-	
+
 		 Extend
-	
+
 	------------------------------------------
-		We need to emulate inheritence to copy over proto 
+		We need to emulate inheritence to copy over proto
 		and static methods without overriding the parents defaults
 
 	*/
@@ -113,18 +113,18 @@
 	      return QS.__super__.constructor.apply(this, arguments);
 	    }
 			if (protoProps) {
-				for (var ckey in protoProps) { 
-					QS.prototype[ckey] = protoProps[ckey]; 
+				for (var ckey in protoProps) {
+					QS.prototype[ckey] = protoProps[ckey];
 				}
 			}
 	    return QS;
 	  })(this);
 	};
-	
+
 	// for any addons
 	QS.extend = extend;
 	QS.Vector2D = Vector2D; // is also global anyways
-		
+
 }).call(this);
 
 'use strict';
@@ -141,7 +141,7 @@ var Path2D = function() {
 };
 
 Path2D.prototype = {
-	
+
 	addLineTo: function(x, y) {
 		return this.points.push(Vector2D.fromValues(x, y));
 	},
@@ -156,7 +156,7 @@ Path2D.prototype = {
 			this.quadBezierTo(sx, sy, hx, hy, ex, ey, i);
 		};
 	},
-	
+
 	// start, handle, end, percent
 	quadBezierTo: function(sx, sy, hx, hy, ex, ey, p) {
 		var x = Math.pow(1 - p, 2) * sx + 2 * (1 - p) * p * hx + Math.pow(p, 2) * ex;
@@ -170,7 +170,7 @@ Path2D.prototype = {
 	    var y = this.cubicN(p, sy, h1y, h2y, ey);
 	    return this.points.push(Vector2D.fromValues(x, y));
 	},
-	
+
 	// calculates cubicN
 	cubicN: function(pct, a, b, c, d) {
 		var t2 = pct * pct, t3 = t2 * pct;
@@ -200,7 +200,7 @@ var Dot = function(parent, position, velocity, scale, maxSpeed, showText, trail)
 	this.lifeSpent = 0;
 	this.showText = showText;
 	this.trail = trail;
-	this.setup();	
+	this.setup();
 };
 
 Dot.prototype = {
@@ -214,18 +214,19 @@ Dot.prototype = {
 		var shape, color;
 
 		this.g = new PIXI.Graphics();
-	
-		if (this.randS === 1) {
-			shape = "circle";			
-		} else if (this.randS === 2) {
-			shape = "square";			
-		} else if (this.randS === 3) {
-			shape = "cross";			
-		} else if (this.randS === 4) {
-			shape = "circle";			
-		} 
 
-		color = this.trail === "trail" ? "red" : "grey";
+		if (this.randS === 1) {
+			shape = "whiteEgg";
+		} else if (this.randS === 2) {
+			shape = "whiteEggWithYolk";
+		} else if (this.randS === 3) {
+			shape = "duckEggWithSpots";
+		} else if (this.randS === 4) {
+			shape = "brownEgg";
+		}
+
+		color = this.trail === "trail" ? "small" : "tiny";
+		console.log("/assets/images/" + shape + "-" + color + ".png");
 		this.g = PIXI.Sprite.fromImage("/assets/images/" + shape + "-" + color + ".png");
 		this._parent.stage.addChild(this.g);
 	},
@@ -287,19 +288,19 @@ Dot.prototype = {
 		this.wanderTheta += Math.random() * (Math.floor(Math.random()*2) == wanderC ? wanderC : -wanderC) - (Math.floor(Math.random()*2) == wanderC ? wanderC : -wanderC) * .5;
 		// Offset the wander theta
 		wanderTarget = [Math.cos(this.wanderTheta) * wanderR, Math.sin(this.wanderTheta) * wanderR];
-		Vector2D.add(wanderTarget, wanderTarget, circlePos);		
+		Vector2D.add(wanderTarget, wanderTarget, circlePos);
 		// seek the target destination
 		return this.seek(wanderTarget, this.position);
 	},
 
 	followPath: function(path, radius) {
-		var predict, predictPos, target, worldRecord, a, b, normalPoint, dir, d; 
+		var predict, predictPos, target, worldRecord, a, b, normalPoint, dir, d;
 		predict = Vector2D.clone(this.velocity);
 		predictPos = Vector2D.create();
 		// predict our future location
 		Vector2D.normalize(predict, predict);
 		Vector2D.scale(predict, predict, radius);
-		// predict our future location		
+		// predict our future location
 		Vector2D.add(predictPos, predictPos, this.position);
 		Vector2D.add(predictPos, predictPos, predict);
 		// define out target and worldrecord
@@ -502,19 +503,19 @@ DotInfo.BG = QS.extend({
 		this.ctx.resize(this.size[0], this.size[1]);
 		this.createTrail();
 	}
-	
+
 });
 
 
 function randomNumber(min, max) {
-	if (max == null) { 
-		max = min; 
+	if (max == null) {
+		max = min;
 		min = 0;
 	}
 	return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 // WHEN LOADED START
-onload = function() {	
+onload = function() {
 	root.dotInfoBG = new root.DotInfo.BG();
 };
